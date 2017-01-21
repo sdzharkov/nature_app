951213e5711a0d3be08f2bf3ac77976851eb3938
sdzharkov <stephan.zharkov@gmail.com>
2017-01-21 14:38:17 -0800

    map

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('map.html')
 
 
 if __name__ == "__main__":
